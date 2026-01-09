8fece1d05c5d15e45545b2643a92e19100f87ed0
Katerynakulik <katerynakulik4@gmail.com>
2026-01-09 02:09:58 +0200

    Change file structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import Home from "@/components/Home/Home"; // Імпортуємо твій компонент
+import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Page() {
   return (
     <main>
-      <Home />
+      <section className={styles.hero}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Campers of your dreams</h1>
+          <p className={styles.subtitle}>
+            You can find everything you want in our catalog
+          </p>
+          <Link href="/catalog" className={styles.button}>
+            View Now
+          </Link>
+        </div>
+      </section>
     </main>
   );
 }
